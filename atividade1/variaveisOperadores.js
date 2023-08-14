80f6f3ba3173141be5a17d19ce57ba137dcd4262
Fábio <80458001+Fabiosantoste@users.noreply.github.com>
2023-08-14 00:35:24 -0300

    adicionado variaveisop

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,15 @@
+var numero1 = 10;
+var numero2 = 5;
+
+
+var soma = numero1 + numero2;
+var subtracao = numero1 - numero2;
+var multiplicacao = numero1 * numero2;
+var divisao = numero1 / numero2;
+var restoDivisao = numero1 % numero2; 
+
+console.log(`Resultado da soma entre ${numero1} e ${numero2} = ${soma}`);
+console.log(`Resultado da subtração entre ${numero1} e ${numero2} = ${subtracao}`);
+console.log(`Resultado da multiplicação entre ${numero1} e ${numero2} = ${multiplicacao}`);
+console.log(`Resultado da divisão entre ${numero1} e ${numero2} = ${divisao}`);
+console.log(`Resultado do resto da divisão entre ${numero1} e ${numero2} = ${restoDivisao}`);
